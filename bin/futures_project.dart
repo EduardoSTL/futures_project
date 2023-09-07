19cf49d49e6eeaf960462ca1d922004012a4c53a
EduardoSTL <eduardojosemelgar2004@gmail.com>
2023-09-07 11:50:30 -0600

    Initial commit

---
+++ bin/futures_project.dart
@@ -0,0 +1,5 @@
+import 'package:futures_project/futures_project.dart' as futures_project;
+
+void main(List<String> arguments) {
+  print('Hello world: ${futures_project.calculate()}!');
+}
---
+++ test/futures_project_test.dart
@@ -0,0 +1,8 @@
+import 'package:futures_project/futures_project.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
