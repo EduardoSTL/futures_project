7e85f6037ce84a3bef08d21f5dc227f839b6f139
EduardoSTL <eduardojosemelgar2004@gmail.com>
2023-09-08 10:30:32 -0600

    updt

--- bin/streams/stream_await.dart
+++ bin/streams/stream_await.dart
@@ -9,6 +9,7 @@ Stream<int> emitNumber() async* {
   final valuesToEmit = [1,2,3,4,5];
   for (int i in valuesToEmit){
     await Future.delayed(const Duration(seconds: 1));
+    //* yield = retornar "esto", hasta complementar (en este caso los elemento de la lista "valuesToEmit")
     yield i;
   }
 }
